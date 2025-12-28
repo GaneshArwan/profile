6694fa1ab05f697a473e78df69df56858dc87b88
GaneshArwan <wiliamzwili135@gmail.com>
2025-12-28 14:54:40 +0700

    feat: add Vercel Analytics and Speed Insights to enhance performance tracking

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ import {
 } from 'lucide-react';
 import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/dist/locomotive-scroll.css'; // Pastikan import CSS jika perlu
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // --- Custom Icons ---
 
 const GithubIcon = ({ size = 24, className = "" }) => (
